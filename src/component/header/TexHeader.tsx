d94fbafad2c1b108b6837a6a0cb54978105f890f
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-08-03 17:58:27 +0800

    refactor: tweak the header

--- src/component/header/TexHeader.tsx
+++ src/component/header/TexHeader.tsx
@@ -1,19 +1,47 @@
 import "@/scss/style.scss";
 import styles from "./TexHeader.module.css";
+import { useNavigate } from "react-router-dom";
 
 const TexHeader: React.FC = () => {
+    const navigate = useNavigate();
+
     return (
         <div className={styles.headerLayout}>
-            <div className="btn btn-primary">登录</div>
-            <div className="dropdown">
-                <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                    语言
-                </button>
-                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                    <li><a className="dropdown-item" href="#">简体中文</a></li>
-                    <li><a className="dropdown-item" href="#">English</a></li>
-                </ul>
-            </div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">TeXHub</a>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">主页</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">模版中心</a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="/doc/tab">个人中心</a>
+                            </li>
+                            <li>
+                                <button className="btn btn-outline-success" type="submit">登录</button>
+                            </li>
+                            <li>
+                                <button className="btn btn-outline-success" type="submit">注册</button>
+                            </li>
+                            <li>
+                                <div className="dropdown">
+                                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                                        语言
+                                    </button>
+                                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                                        <li><a className="dropdown-item" href="#">简体中文</a></li>
+                                        <li><a className="dropdown-item" href="#">English</a></li>
+                                    </ul>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         </div>
     );
 }
