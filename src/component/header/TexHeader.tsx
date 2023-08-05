c4729cced70b906131cd16f485305a5955e9dd76
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-08-05 19:16:47 +0800

    style: tweak the header

--- src/component/header/TexHeader.tsx
+++ src/component/header/TexHeader.tsx
@@ -9,7 +9,7 @@ const TexHeader: React.FC = () => {
         <div className={styles.headerLayout}>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="#">TeXHub</a>
+                    <a className="navbar-brand" href="/">TeXHub</a>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
@@ -21,27 +21,27 @@ const TexHeader: React.FC = () => {
                             <li className="nav-item">
                                 <a className="nav-link" href="/doc/tab">个人中心</a>
                             </li>
-                            <li>
-                                <button className="btn btn-outline-success" type="submit">登录</button>
-                            </li>
-                            <li>
-                                <button className="btn btn-outline-success" type="submit">注册</button>
-                            </li>
-                            <li>
-                                <div className="dropdown">
-                                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                                        语言
-                                    </button>
-                                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                        <li><a className="dropdown-item" href="#">简体中文</a></li>
-                                        <li><a className="dropdown-item" href="#">English</a></li>
-                                    </ul>
-                                </div>
-                            </li>
                         </ul>
                     </div>
                 </div>
             </nav>
+            <div className={styles.headerOperation}>
+                <div>
+                    <button className="btn btn-outline-success" type="submit">登录</button>
+                </div>
+                <div>
+                    <button className="btn btn-outline-success" type="submit">注册</button>
+                </div>
+                <div className="dropdown">
+                    <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                        语言
+                    </button>
+                    <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a className="dropdown-item" href="#">简体中文</a></li>
+                        <li><a className="dropdown-item" href="#">English</a></li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 }
