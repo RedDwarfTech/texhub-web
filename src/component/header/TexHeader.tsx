5af1768bb346eb3ee5ca204da87f1f6cd6ce7022
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-08-05 21:39:47 +0800

    style: tweak the header to dark color

--- src/component/header/TexHeader.tsx
+++ src/component/header/TexHeader.tsx
@@ -7,19 +7,19 @@ const TexHeader: React.FC = () => {
 
     return (
         <div className={styles.headerLayout}>
-            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div className="container-fluid">
-                    <a className="navbar-brand" href="/">TeXHub</a>
+                    <a className="navbar-brand text-light" href="/">TeXHub</a>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">主页</a>
+                                <a className="nav-link active text-light" aria-current="page" href="/">主页</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/tpl">模版中心</a>
+                                <a className="nav-link text-light" href="/tpl">模版中心</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/doc/tab">个人中心</a>
+                                <a className="nav-link text-light" href="/doc/tab">个人中心</a>
                             </li>
                         </ul>
                     </div>
@@ -27,10 +27,10 @@ const TexHeader: React.FC = () => {
             </nav>
             <div className={styles.headerOperation}>
                 <div>
-                    <button className="btn btn-outline-success" type="submit">登录</button>
+                    <button className="btn btn-outline-info text-light" type="submit">登录</button>
                 </div>
                 <div>
-                    <button className="btn btn-outline-success" type="submit">注册</button>
+                    <button className="btn btn-outline-info text-light" type="submit">注册</button>
                 </div>
                 <div className="dropdown">
                     <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
