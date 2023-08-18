6dc3dc9803602018188f8f968c7b6c59047be606
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-08-18 21:52:42 +0800

    feat: tweak the tex public servcie url

--- src/config/app/config-pro.ts
+++ src/config/app/config-pro.ts
@@ -1,13 +1,13 @@
 let proConfigMap = new Map([
   ["wssUrl", "wss://tex.poemhub.top/post/websocket"],
   ["logoutUrl", "https://tex.poemhub.top"],
-  ["loginUrl", "/tex/user/login"],
-  ["regUrl", "/tex/user/reg"],
+  ["loginUrl", "/texpub/user/login"],
+  ["regUrl", "/texpub/user/reg"],
   ["baseAuthUrl","https://tex.poemhub.top"],
-  ["accessTokenUrlPath","/tex/auth/access-token/refresh"],
+  ["accessTokenUrlPath","/texpub/auth/access-token/refresh"],
   ["appHome","https://tex.poemhub.top"],
   ["appId" , "n29Pa29WS1"],
-  ["refreshUserUrl","/tex/user/current-user"],
+  ["refreshUserUrl","/texpub/user/current-user"],
   ["compileBaseUrl","https://tex.poemhub.top/tex/static"]
 ]); 
 
