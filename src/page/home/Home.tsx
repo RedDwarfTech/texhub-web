571955bd01b9f8ca4ddba90824118138910b3c9b
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-08-24 12:25:03 +0800

    fix: the using latex navigation

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -14,7 +14,7 @@ const Home: React.FC = () => {
                     <div className={styles.overviewContent}>
                         <h1>在线LaTeX</h1>
                         <h3>告别繁琐的安装，开箱即用</h3>
-                        <button className="btn btn-primary" onClick={() => { navigate('/user/cv/list'); }}>立即体验</button>
+                        <button className="btn btn-primary" onClick={() => { navigate('/doc/tab'); }}>立即体验</button>
                     </div>
                 </div>
                 <div className={styles.template}>
