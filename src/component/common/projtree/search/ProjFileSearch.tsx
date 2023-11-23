b0ed422d12aba0afdbaacb5494ce2f2d5f3298af
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-11-23 16:55:29 +0800

    refactor: tweak the match content

--- src/component/common/projtree/search/ProjFileSearch.tsx
+++ src/component/common/projtree/search/ProjFileSearch.tsx
@@ -53,9 +53,7 @@ const ProjFileSearch: React.FC<ProjSearchProps> = (props: ProjSearchProps) => {
                         {hitItem[i].name}
                     </div>
                     <div>
-                        <i>- 匹配项1</i>
-                        <i>- 匹配项2</i>
-                        <i>- 匹配项3</i>
+                        <i>{hitItem[i].cotent}</i>
                     </div>
                 </div>
             );
