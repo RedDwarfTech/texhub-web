e90d4a5cd5d2b36496dfc0337cd3ed96cc7f090c
jiang qiang <jiangtingqiang@gmail.com>
2024-01-04 14:37:02 +0800

    refactor: change the cmaps cdn to local website

--- src/component/common/previewer/Previewer.tsx
+++ src/component/common/previewer/Previewer.tsx
@@ -119,7 +119,7 @@ const Previewer: React.FC<PreviwerProps> = ({ projectId, viewModel }) => {
     }, [streamLogText]);
 
     const options: Options = {
-        cMapUrl: `https://cdn.jsdelivr.net/npm/pdfjs-dist@${pdfjs.version}/cmaps/`,
+        cMapUrl: `/pdfjs-dist@${pdfjs.version}/cmaps/`,
         httpHeaders: {
             'Authorization': 'Bearer ' + getAccessToken()
         }
