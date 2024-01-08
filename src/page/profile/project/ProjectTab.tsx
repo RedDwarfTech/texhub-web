43301404e9912338e594a8f7b4a3e9bf588c64e0
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-01-08 16:59:51 +0800

    feat: add collapse demo

--- src/page/profile/project/ProjectTab.tsx
+++ src/page/profile/project/ProjectTab.tsx
@@ -36,7 +36,6 @@ const ProjectTab: React.FC = () => {
     const [activeTab, setActiveTab] = useState<ProjTabType>(ProjTabType.All);
     const { projList, folderProjList } = useSelector((state: AppState) => state.proj);
     const delProjCancelRef = useRef<HTMLButtonElement>(null);
-
     const navigate = useNavigate();
     const { t } = useTranslation();
     const { i18n } = useTranslation();
@@ -241,7 +240,6 @@ const ProjectTab: React.FC = () => {
         if (!curProjMap || curProjMap.length === 0) {
             return (<div></div>);
         }
-        debugger
         let projList = renderProj(curProjMap);
         return projList;
     }
@@ -394,6 +392,51 @@ const ProjectTab: React.FC = () => {
     return (
         <div>
             <TexHeader></TexHeader>
+            <div id="accordion">
+  <div className="card">
+    <div className="card-header" id="headingOne">
+      <h5 className="mb-0">
+        <button className="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+          Collapsible Group Item #1
+        </button>
+      </h5>
+    </div>
+
+    <div id="collapseOne" className="collapse show" aria-labelledby="headingOne" data-parent="#accordion">
+      <div className="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div className="card">
+    <div className="card-header" id="headingTwo">
+      <h5 className="mb-0">
+        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+          Collapsible Group Item #2
+        </button>
+      </h5>
+    </div>
+    <div id="collapseTwo" className="collapse" aria-labelledby="headingTwo" data-parent="#accordion">
+      <div className="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+  <div className="card">
+    <div className="card-header" id="headingThree">
+      <h5 className="mb-0">
+        <button className="btn btn-link collapsed" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+          Collapsible Group Item #3
+        </button>
+      </h5>
+    </div>
+    <div id="collapseThree" className="collapse" aria-labelledby="headingThree" data-parent="#accordion">
+      <div className="card-body">
+        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+      </div>
+    </div>
+  </div>
+</div>
             <div className={styles.projBody}>
                 <div className={styles.projList}>
                     <ul className="nav nav-tabs">
