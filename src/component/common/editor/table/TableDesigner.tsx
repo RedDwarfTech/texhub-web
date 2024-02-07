8188b9e1f00edfe00faea02f876c9589fc718def
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-02-07 15:43:35 +0800

    feat: add icon for table designer

--- src/component/common/editor/table/TableDesigner.tsx
+++ src/component/common/editor/table/TableDesigner.tsx
@@ -1,4 +1,13 @@
-import { ReactComponent as PlusColumn } from '@/assets/icon/table-column-plus-before.svg';
+import { ReactComponent as PlusColumnBefore } from '@/assets/icon/table-column-plus-before.svg';
+import { ReactComponent as PlusColumnAfter } from '@/assets/icon/table-column-plus-after.svg';
+import { ReactComponent as PlusColumnRemove } from '@/assets/icon/table-column-remove.svg';
+import { ReactComponent as PlusRowBefore } from '@/assets/icon/table-row-plus-before.svg';
+import { ReactComponent as PlusRowAfter } from '@/assets/icon/table-row-plus-after.svg';
+import { ReactComponent as PlusRowRemove } from '@/assets/icon/table-row-remove.svg';
+import { ReactComponent as BorderBottom } from '@/assets/icon/border-bottom.svg';
+import { ReactComponent as BorderTop } from '@/assets/icon/border-top.svg';
+import { ReactComponent as BorderLeft } from '@/assets/icon/border-left.svg';
+import { ReactComponent as BorderRight } from '@/assets/icon/border-right.svg';
 import styles from "./TableDesigner.module.css";
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
@@ -29,10 +38,29 @@ const TableDesigner: React.FC<TableDesignerProps> = (props: TableDesignerProps) 
                         <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                     </div>
                     <div className="modal-body">
-                        <div>
-                            <div className={styles.modalAddRow}>
-                                <PlusColumn width={'20px'} height={'20px'}></PlusColumn>
-                            </div>
+                        <div className={styles.tableAction}>
+                            <table>
+                                <tr>
+                                    <th><PlusRowBefore width={'20px'} height={'20px'}></PlusRowBefore></th>
+                                    <th><PlusRowAfter width={'20px'} height={'20px'}></PlusRowAfter></th>
+                                    <th><PlusRowRemove width={'20px'} height={'20px'}></PlusRowRemove></th>
+                                </tr>
+                            </table>
+                            <table>
+                                <tr>
+                                    <th><PlusColumnBefore width={'20px'} height={'20px'}></PlusColumnBefore></th>
+                                    <th><PlusColumnAfter width={'20px'} height={'20px'}></PlusColumnAfter></th>
+                                    <th><PlusColumnRemove width={'20px'} height={'20px'}></PlusColumnRemove></th>
+                                </tr>
+                            </table>
+                            <table>
+                                <tr>
+                                    <th><BorderBottom width={'20px'} height={'20px'}></BorderBottom></th>
+                                    <th><BorderTop width={'20px'} height={'20px'}></BorderTop></th>
+                                    <th><BorderLeft width={'20px'} height={'20px'}></BorderLeft></th>
+                                    <th><BorderRight width={'20px'} height={'20px'}></BorderRight></th>
+                                </tr>
+                            </table>
                             <div className="modal-col"></div>
                         </div>
                         <hr />
