e4f5983d123779a45af685d8f56002339051d641
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-02-11 21:40:41 +0800

    feat: remove redux logger

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,14 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import rootReducer from '@/redux/reducer/combineReducer';
-import { createLogger } from 'redux-logger';
 import thunk from 'redux-thunk';
 
-const logger = createLogger(); 
 const initialState = {};
 
 const store = configureStore({
     reducer: rootReducer,
-    middleware: [logger, thunk],
+    middleware: [thunk],
     devTools: process.env.NODE_ENV !== 'production',
     preloadedState: initialState
 });
