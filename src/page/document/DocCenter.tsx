da57328ccb302deaf6e4daf2bf85df4a3fda39ee
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-02-17 20:01:06 +0800

    feat: add latex tutorial

--- src/page/document/DocCenter.tsx
+++ src/page/document/DocCenter.tsx
@@ -14,6 +14,9 @@ const DocCenter: React.FC = () => {
                     <div className={styles.docItem}>
                         <a target="_blank" href="https://texdoc.org/serve/lshort-zh-cn.pdf/0">一份不太简短的LaTeX介绍</a>
                     </div>
+                    <div className={styles.docItem}>
+                        <a target="_blank" href="https://search.bilibili.com/all?vt=70768055&keyword=LaTeX%E6%95%99%E7%A8%8B&from_source=webtop_search&spm_id_from=&search_source=5">LaTeX教程</a>
+                    </div>
                 </div>
             </div>
         </div>
