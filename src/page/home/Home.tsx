37ec9b159f01817e789920b531cb405111cc03c3
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-02-17 19:44:37 +0800

    docs: tweak the home slogan

--- src/page/home/Home.tsx
+++ src/page/home/Home.tsx
@@ -1,10 +1,10 @@
 import TexHeader from "@/component/header/TexHeader";
 import styles from "./Home.module.css";
 import { useNavigate } from "react-router-dom";
-import moderncv from "@/assets/cv/template/moderncv-legacy-template-zh.png";
 import onlineEditor from "@/assets/cv/template/tex-online-editor-compress.jpg";
 import userProjects from "@/assets/cv/template/user-projects.jpg";
-import { UserService } from "rd-component";
+import onlineCollarboration from "@/assets/cv/template/online-collaboration.jpg";
+import { Footer, UserService } from "rd-component";
 import { toast, ToastContainer } from 'react-toastify';
 
 const Home: React.FC = () => {
@@ -45,19 +45,24 @@ const Home: React.FC = () => {
                     </div>
                     <div className={styles.opRight}>
                         <h1>不止论文</h1>
-                        <h4>一切有价值、需要长期保存的记忆</h4>
+                        <h4>一切有价值、需要长期保存的内容</h4>
+                        <h4>乐谱、请帖、信件、笔记、书......</h4>
                     </div>
                 </div>
                 <div className={styles.template}>
-                    <div className={styles.tplRight}>
-                        <img src={moderncv} style={{width: '500px',height: '700px'}} alt="Image 2" />
-                    </div>
                     <div className={styles.opRight}>
                         <h1>在线协作</h1>
                         <h4>同时编辑文档，并行不悖</h4>
                     </div>
+                    <div className={styles.tplRight}>
+                        <img src={onlineCollarboration} alt="Image 2" />
+                    </div>
+                </div>
+                <div className={styles.buttonDiv}>
+
                 </div>
             </div>
+            <Footer></Footer>
             <ToastContainer />
         </div>
     );
