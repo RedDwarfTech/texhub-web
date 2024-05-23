a7726557a1fee9d1b7785d2b47d4bcb842fc4a01
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-05-23 04:25:27 +0000

    [fix] import the project share reducer

--- src/redux/reducer/combineReducer.ts
+++ src/redux/reducer/combineReducer.ts
@@ -4,13 +4,15 @@ import proj from "@/redux/reducer/project/ProjectReducer";
 import tpl from "@/redux/reducer/tpl/TemplateReducer";
 import file from "@/redux/reducer/file/FileReducer";
 import snippet from "@/redux/reducer/snippet/SnippetReducer";
+import projShare from "@/redux/reducer/project/ProjectShareReducer";
 
 const rootReducer = combineReducers({
     proj,
     rdRootReducer,
     tpl,
     file,
-    snippet
+    snippet,
+    projShare
 });
 
 export default rootReducer;
