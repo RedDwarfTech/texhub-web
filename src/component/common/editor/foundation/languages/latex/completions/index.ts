21313f8cf93d611815f60fe6c569913bdf98fc75
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-08-25 15:42:40 +0800

    feat: add code completion

--- src/component/common/editor/foundation/languages/latex/completions/index.ts
+++ src/component/common/editor/foundation/languages/latex/completions/index.ts
@@ -1,6 +1,10 @@
+import { buildBibliographyStyleCompletions } from "./bibliography-styles";
 import { buildClassCompletions } from "./classes";
 import { buildEnvironmentCompletions } from "./environments";
+import { buildIncludeCompletions } from "./include";
+import { buildLabelCompletions } from "./labels";
 import { buildPackageCompletions } from "./packages";
+import { buildReferenceCompletions } from "./references";
 import { buildSnippetCompletions } from "./snippets";
 import { Completions } from "./types";
 import { CompletionContext } from "@codemirror/autocomplete";
@@ -12,6 +16,10 @@ export const buildAllCompletions = (
   buildSnippetCompletions(completions);
   buildEnvironmentCompletions(completions);
   buildClassCompletions(completions);
+  buildBibliographyStyleCompletions(completions);
+  buildIncludeCompletions(completions, context);
+  buildReferenceCompletions(completions, context);
+  buildLabelCompletions(completions, context);
   buildPackageCompletions(completions, context);
   return completions;
 };
