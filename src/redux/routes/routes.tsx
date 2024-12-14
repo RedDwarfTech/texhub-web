03247bb09f083ee6ad78e63042127e00984e1369
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-12-14 18:14:06 +0800

    refactor: tweak the react route future config

--- src/redux/routes/routes.tsx
+++ src/redux/routes/routes.tsx
@@ -99,6 +99,9 @@ const routes = createBrowserRouter(routeDefine,{
   future: {
     v7_normalizeFormMethod: true,
     v7_relativeSplatPath: true,
+    v7_fetcherPersist: true,
+    v7_partialHydration: true,
+    v7_skipActionErrorRevalidation: true,
   }
 });
 
