e85ca869022f46c8ec817bee74dd1bf3a740a0e3
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-12-18 21:46:33 +0800

    refactor: remove the unused code

--- src/component/common/previewer/doc/TeXPDFPage.tsx
+++ src/component/common/previewer/doc/TeXPDFPage.tsx
@@ -7,12 +7,10 @@ import { useSelector } from "react-redux";
 import { AppState } from "@/redux/types/AppState";
 import {
   getCurPdfScale,
-  setCurPdfPage,
 } from "@/service/project/preview/PreviewService";
 import { PageViewport } from "pdfjs-dist";
 import { PdfPosition } from "@/model/proj/pdf/PdfPosition";
 import TeXPDFHighlight from "../feat/highlight/TeXPDFHighlight";
-import { isMoreThanFiveSeconds } from "./PDFPreviewHandle";
 
 interface PDFPageProps {
   index: number;
@@ -49,8 +47,6 @@ const TeXPDFPage: React.FC<PDFPageProps> = ({
       canvasArray.current[index] = { current: element };
     }
   };
-  const [renderedPageNumber, setRenderedPageNumber] = useState<number>(-1);
-  const [renderedScale, setRenderedScale] = useState(1);
 
   React.useEffect(() => {
     if (projAttr.pdfScale === 1 && cachedScale) {
@@ -60,49 +56,10 @@ const TeXPDFPage: React.FC<PDFPageProps> = ({
   }, [projAttr, cachedScale]);
 
   const handlePageRenderSuccess = (page: PageCallback) => {
-    // setRenderedScale(projAttribute.pdfScale);
-    // setRenderedPageNumber(page.pageNumber);    
+       
   };
 
   const handlePageChange = (page: any) => {};
-
-  /**
-   * to avoid the pdf page flashing & flicking when zoom the pdf
-   * keep the legacy page before the new pdf page rendered success
-   * https://github.com/wojtekmaj/react-pdf/issues/875
-   * https://github.com/wojtekmaj/react-pdf/issues/418
-   *
-   * @param page
-   */
-  const renderLegacyPage = (pageNumber: number, width: number) => {
-    if (isLoading) {
-      return (
-        <Page
-          /**
-           * IMPORTANT: Keys are necessary so that React will know which Page component
-           * instances to use.
-           * Without keys, on page number update, React would replace the page number
-           * in 1st and 2nd page components. This may cause previously rendered page
-           * to render again, thus causing a flash.
-           * With keys, React, will add prevPage className to previously rendered page,
-           * and mount new Page component instance for the new page.
-           */
-          key={pageNumber + "@legacy-" + projAttribute.legacyPdfScale}
-          className="prevPage"
-          scale={projAttribute.legacyPdfScale}
-          pageNumber={pageNumber}
-          onChange={handlePageChange}
-          width={width}
-        />
-      );
-    } else {
-      console.log("page" + pageNumber + " has rendered");
-      return null;
-    }
-  };
-
-  const isLoading =
-    renderedPageNumber !== index || renderedScale !== projAttribute.pdfScale;
 
   return (
     <div
