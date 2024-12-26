68831198d8dfe7761dbbe01105cfc59728aec8c0
JiangXiaoqiang <jiangtingqiang@gmail.com>
2024-12-26 23:03:07 +0800

    feat: try to fix the dead loop of interval

--- src/component/header/editor/AppHeader.tsx
+++ src/component/header/editor/AppHeader.tsx
@@ -3,17 +3,17 @@ import styles from "./AppHeader.module.css";
 import { AppState } from "@/redux/types/AppState";
 import React, { useState } from "react";
 import { TexFileModel } from "@/model/file/TexFileModel";
-import { toast } from 'react-toastify';
+import { toast } from "react-toastify";
 import {
-    clearCompLogText,
-    compileProjectLog,
-    getStreamLog,
-    getCompQueueStatus,
-    sendQueueCompileRequest, 
-    setCompileStatus, 
-    showPreviewTab, 
-    updateLogText,
-    projHistoryPage
+  clearCompLogText,
+  compileProjectLog,
+  getStreamLog,
+  getCompQueueStatus,
+  sendQueueCompileRequest,
+  setCompileStatus,
+  showPreviewTab,
+  updateLogText,
+  projHistoryPage,
 } from "@/service/project/ProjectService";
 import { useNavigate } from "react-router-dom";
 import { CompileQueueReq } from "@/model/request/proj/CompileQueueReq";
@@ -28,151 +28,170 @@ import ProjHistory from "@/page/main/history/ProjHistory";
 import { QueryHistory } from "@/model/request/proj/query/QueryHistory";
 
 const EHeader: React.FC = () => {
+  const { fileTree } = useSelector((state: AppState) => state.file);
+  const { queue, projInfo } = useSelector((state: AppState) => state.proj);
+  const [mainFile, setMainFile] = useState<TexFileModel>();
+  const navigate = useNavigate();
+  const { t } = useTranslation();
 
-    const { fileTree } = useSelector((state: AppState) => state.file);
-    const { queue, projInfo } = useSelector((state: AppState) => state.proj);
-    const [mainFile, setMainFile] = useState<TexFileModel>();
-    const navigate = useNavigate();
-    const { t } = useTranslation();
-
-    React.useEffect(() => {
-        if (fileTree && fileTree.length > 0) {
-            let defaultFile = fileTree.filter((file: TexFileModel) => file.main_flag === 1);
-            setMainFile(defaultFile[0]);
-        }
-    }, [fileTree]);
-
-    React.useEffect(() => {
-        if (projInfo && Object.keys(projInfo).length > 0) {
-            setMainFile(projInfo.main_file);
-        }
-    }, [projInfo]);
-
-    React.useEffect(() => {
-        let interval: NodeJS.Timeout | null = null;
-        if (queue && Object.keys(queue).length > 0) {
-            if (queue.comp_status === CompileStatus.WAITING) {
-                if(interval === null) {
-                    interval = setInterval(() => {
-                        getCompQueueStatus(queue.id);
-                    }, 5000);
-                }
-            }
-            if (queue.comp_status !== 0 && interval) {
-                clearInterval(interval);
-                return;
-            }
-            if (!mainFile) {
-                return;
-            }
-            let req: CompileProjLog = {
-                project_id: mainFile.project_id,
-                file_name: mainFile.name,
-                version_no: queue.version_no,
-                qid: queue.id,
-                access_token: getAccessToken()
-            };
-            if (queue.comp_status === CompileStatus.COMPILING) {
-                clearCompileCheck(interval);
-                getStreamLog(req, onSseMessage);
-            }
-            if (queue.comp_status === CompileStatus.COMPLETE) {
-                clearCompileCheck(interval);
-                compileProjectLog(req);
-            }
-            return () => {
-                clearCompileCheck(interval);
-            };
-        }
-    }, [queue]);
-
-    const clearCompileCheck = (interval: NodeJS.Timeout | null) => {
-        if (interval) {
-            clearInterval(interval);
-        }
+  React.useEffect(() => {
+    if (fileTree && fileTree.length > 0) {
+      let defaultFile = fileTree.filter(
+        (file: TexFileModel) => file.main_flag === 1
+      );
+      setMainFile(defaultFile[0]);
     }
+  }, [fileTree]);
 
-    const handleQueueCompile = (mainFile: TexFileModel) => {
-        if (!mainFile) {
-            toast.error("file is null");
-        }
-        let req: CompileQueueReq = {
-            project_id: mainFile.project_id
-        };
-        sendQueueCompileRequest(req).then((res) => {
-            if (ResponseHandler.responseSuccess(res)) {
-                showPreviewTab("logview");
-                setCompileStatus(CompileStatus.WAITING);
-                clearCompLogText("====CLEAR====");
-            }
-        });
+  React.useEffect(() => {
+    if (projInfo && Object.keys(projInfo).length > 0) {
+      setMainFile(projInfo.main_file);
     }
+  }, [projInfo]);
 
-    const handleSettings = (mainFile: TexFileModel) => {
-
-    }
-
-    const handleProjHist = (mainFile: TexFileModel) => {
-        const hist: QueryHistory = {
-            project_id: mainFile.project_id
-        };
-        projHistoryPage(hist);
-    }
-
-    const onSseMessage = (msg: string, eventSource: EventSource) => {
-        updateLogText(msg);
-    }
-
+  React.useEffect(() => {
     if (!mainFile) {
-        return <div>Loading...</div>
+      return;
     }
-
-    const handleNavProfile = () => {
-        navigate('/doc/tab');
+    let interval: NodeJS.Timeout | null = null;
+    if (queue && Object.keys(queue).length > 0) {
+      if (queue.comp_status !== 0 && interval) {
+        clearInterval(interval);
+        return;
+      }
+      let req: CompileProjLog = {
+        project_id: mainFile.project_id,
+        file_name: mainFile.name,
+        version_no: queue.version_no,
+        qid: queue.id,
+        access_token: getAccessToken(),
+      };
+      if (queue.comp_status === CompileStatus.WAITING) {
+        if (interval === null) {
+          interval = setInterval(() => {
+            getCompQueueStatus(queue.id);
+          }, 5000);
+        }
+      } else if (queue.comp_status === CompileStatus.COMPILING) {
+        clearCompileCheck(interval);
+        getStreamLog(req, onSseMessage);
+      } else if (queue.comp_status === CompileStatus.COMPLETE) {
+        clearCompileCheck(interval);
+        compileProjectLog(req);
+      } else {
+        clearCompileCheck(interval);
+      }
+      return () => {
+        clearCompileCheck(interval);
+      };
     }
+  }, [queue]);
 
-    return (
-        <div className={styles.container}>
-            <div>
-                <TeXShare projectId={mainFile.project_id}></TeXShare>
-                <ProjHistory projectId={mainFile.project_id}></ProjHistory>
-                <ProjSetting></ProjSetting>
-            </div>
-            <div className={styles.actions}>
-                <button type="button" 
-                    className="btn btn-primary btn-sm" 
-                    onClick={() => { handleQueueCompile(mainFile) }}>
-                    <i className="fa-solid fa-play"></i> { t("btn_compile") }
-                </button>
-                <button type="button" 
-                    className="btn btn-primary btn-sm" 
-                    data-bs-toggle="modal"
-                    data-bs-target="#sharePrj"
-                    onClick={() => {}}>
-                    <i className="fa-solid fa-share-nodes"></i> { t("btn_share") }
-                </button>
-                <button type="button"
-                    className="btn btn-primary btn-sm"
-                    data-bs-toggle="offcanvas"
-                    data-bs-target="#projHistory"
-                    aria-controls="offcanvasExample"
-                    onClick={() => { handleProjHist(mainFile) }}>
-                    <i className="fa-solid fa-timeline"></i> { t("btn_history") }
-                </button>
-                <button type="button"
-                    className="btn btn-primary btn-sm"
-                    data-bs-toggle="offcanvas"
-                    data-bs-target="#offcanvasExample"
-                    aria-controls="offcanvasExample"
-                    onClick={() => { handleSettings(mainFile) }}>
-                    <i className="fa-solid fa-cog"></i> { t("btn_settings") }
-                </button>
-                <button type="button" className="btn btn-primary btn-sm" onClick={() => { handleNavProfile() }}>
-                    <i className="fa-solid fa-user"></i> { t("projects") }
-                </button>
-            </div>         
-        </div>
-    );
-}
+  const clearCompileCheck = (interval: NodeJS.Timeout | null) => {
+    if (interval) {
+      clearInterval(interval);
+    }
+  };
+
+  const handleQueueCompile = (mainFile: TexFileModel) => {
+    if (!mainFile) {
+      toast.error("file is null");
+    }
+    let req: CompileQueueReq = {
+      project_id: mainFile.project_id,
+    };
+    sendQueueCompileRequest(req).then((res) => {
+      if (ResponseHandler.responseSuccess(res)) {
+        showPreviewTab("logview");
+        setCompileStatus(CompileStatus.WAITING);
+        clearCompLogText("====CLEAR====");
+      }
+    });
+  };
+
+  const handleSettings = (mainFile: TexFileModel) => {};
+
+  const handleProjHist = (mainFile: TexFileModel) => {
+    const hist: QueryHistory = {
+      project_id: mainFile.project_id,
+    };
+    projHistoryPage(hist);
+  };
+
+  const onSseMessage = (msg: string, eventSource: EventSource) => {
+    updateLogText(msg);
+  };
+
+  if (!mainFile) {
+    return <div>Loading...</div>;
+  }
+
+  const handleNavProfile = () => {
+    navigate("/doc/tab");
+  };
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <TeXShare projectId={mainFile.project_id}></TeXShare>
+        <ProjHistory projectId={mainFile.project_id}></ProjHistory>
+        <ProjSetting></ProjSetting>
+      </div>
+      <div className={styles.actions}>
+        <button
+          type="button"
+          className="btn btn-primary btn-sm"
+          onClick={() => {
+            handleQueueCompile(mainFile);
+          }}
+        >
+          <i className="fa-solid fa-play"></i> {t("btn_compile")}
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary btn-sm"
+          data-bs-toggle="modal"
+          data-bs-target="#sharePrj"
+          onClick={() => {}}
+        >
+          <i className="fa-solid fa-share-nodes"></i> {t("btn_share")}
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary btn-sm"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#projHistory"
+          aria-controls="offcanvasExample"
+          onClick={() => {
+            handleProjHist(mainFile);
+          }}
+        >
+          <i className="fa-solid fa-timeline"></i> {t("btn_history")}
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary btn-sm"
+          data-bs-toggle="offcanvas"
+          data-bs-target="#offcanvasExample"
+          aria-controls="offcanvasExample"
+          onClick={() => {
+            handleSettings(mainFile);
+          }}
+        >
+          <i className="fa-solid fa-cog"></i> {t("btn_settings")}
+        </button>
+        <button
+          type="button"
+          className="btn btn-primary btn-sm"
+          onClick={() => {
+            handleNavProfile();
+          }}
+        >
+          <i className="fa-solid fa-user"></i> {t("projects")}
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default EHeader;
