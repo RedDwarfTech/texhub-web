af54c61c923942fbab10177d9d4f526bfdce7ee5
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-02-09 22:08:08 +0800

    feat: add socket io path

--- src/service/editor/CollarEditorSocketIOService.ts
+++ src/service/editor/CollarEditorSocketIOService.ts
@@ -71,6 +71,7 @@ const doSocketIOConn = (ydoc: Y.Doc, editorAttr: EditorAttr): any => {
   }
   let options: Partial<ManagerOptions & SocketOptions> = {
     withCredentials: true,
+    path: "/socket.io/"
   };
   const wsProvider: any = new SocketIOClientProvider(
     readConfig("socketUrl"),
