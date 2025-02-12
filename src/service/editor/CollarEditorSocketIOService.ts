8134a244d2bc32977329f409fd2c4cc07b654464
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-02-13 00:03:11 +0800

    refactor: tweak the namespace

--- src/service/editor/CollarEditorSocketIOService.ts
+++ src/service/editor/CollarEditorSocketIOService.ts
@@ -71,7 +71,7 @@ const doSocketIOConn = (ydoc: Y.Doc, editorAttr: EditorAttr): any => {
   }
   let options: Partial<ManagerOptions & SocketOptions> = {
     withCredentials: true,
-    path: "/socket.io/"
+    path: "/texhub"
   };
   const wsProvider: any = new SocketIOClientProvider(
     readConfig("socketUrl"),
