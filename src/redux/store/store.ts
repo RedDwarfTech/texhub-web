9007bf98c813b9c355938100bd8e4f3212f2d4ed
JiangXiaoqiang <jiangtingqiang@gmail.com>
2025-10-30 04:06:18 +0000

    [refactor] add initial sse processor logic

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,17 +1,26 @@
 import { configureStore } from "@reduxjs/toolkit";
 import rootReducer from "@/redux/reducer/combineReducer";
+import { initSSEBatchProcessor } from '@/redux/store/middleware/SSEMiddleware';
 
 const initialState: any = {};
 
-const store = configureStore({
-  reducer: rootReducer,
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      immutableCheck: false,
-      serializableCheck: false,
-    }),
-  devTools: process.env.NODE_ENV !== "production",
-  preloadedState: initialState,
-});
+export const createStore = () => {
+  const store = configureStore({
+    reducer: rootReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        immutableCheck: false,
+        serializableCheck: false,
+      }),
+    devTools: process.env.NODE_ENV !== "production",
+    preloadedState: initialState,
+  });
 
+  // 初始化 SSE Batch Processor
+  initSSEBatchProcessor(store);
+  
+  return store;
+};
+
+const store = createStore();
 export default store;
