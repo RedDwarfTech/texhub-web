afa690caa08d83bf6766ae79c3693c81089a99e6
JiangXiaoqiang <jiangtingqiang@gmail.com>
2026-08-09 17:30:57 +0800

    [refact] restore full file menu, delete item to the end

--- src/component/common/projtree/main/ProjectTree.tsx
+++ src/component/common/projtree/main/ProjectTree.tsx
@@ -335,6 +335,36 @@ const ProjectTree: React.FC<TreeProps> = (props: TreeProps) => {
                     <div
                       className="dropdown-item"
                       onClick={(e) => {
+                        handleModal(e, true, "renameFileModal", item);
+                      }}
+                    >
+                      {t("btn_rename")}
+                    </div>
+                  </li>
+                  <li>
+                    <div
+                      className="dropdown-item"
+                      onClick={(e) => {
+                        handleDownloadFile(item);
+                      }}
+                    >
+                      {t("btn_download")}
+                    </div>
+                  </li>
+                  <li>
+                    <div
+                      className="dropdown-item"
+                      onClick={(e) => {
+                        handleModal(e, true, "moveFileModal", item);
+                      }}
+                    >
+                      {t("btn_move_to_folder")}
+                    </div>
+                  </li>
+                  <li>
+                    <div
+                      className="dropdown-item"
+                      onClick={(e) => {
                         showHistory(item);
                       }}
                     >
@@ -349,6 +379,16 @@ const ProjectTree: React.FC<TreeProps> = (props: TreeProps) => {
                       }}
                     >
                       {t("btn_show_info")}
+                    </div>
+                  </li>
+                  <li>
+                    <div
+                      className="dropdown-item"
+                      onClick={(e) => {
+                        handleModal(e, true, "deleteFileModal", item);
+                      }}
+                    >
+                      {t("btn_del")}
                     </div>
                   </li>
                 </ul>
